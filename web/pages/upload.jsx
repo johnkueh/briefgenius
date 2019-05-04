12483143fac8c5cb7f13cf646e084bd9931f5d2c
John Kueh <yoongfook@gmail.com>
2019-05-04 18:58:37 +1000

    Added useUpload hook for cloudinary updates

--- web/pages/upload.jsx
+++ web/pages/upload.jsx
@@ -1,53 +1,21 @@
-const Upload = () => (
-  <div>
-    <button
-      onClick={() => {
-        cloudinary.openUploadWidget(widgetOptions, (err, info) => {
-          if (!err) if (info.event === 'success') handleSuccess(info);
-        });
-      }}
-    >
-      Upload
-    </button>
-  </div>
-);
+import { useUpload } from '../lib/use-upload';
 
-const handleSuccess = ({ info: { public_id } }) => {
-  console.log('handleSuccess', public_id);
+const Upload = () => {
+  const onSuccess = data => {
+    console.log('onSuccess', data);
+  };
+
+  const { openWidget } = useUpload(onSuccess);
+
+  return (
+    <div>
+      <button onClick={openWidget}>Upload</button>
+    </div>
+  );
 };
 
-const widgetOptions = {
-  cloudName: process.env.CLOUDINARY_CLOUD_NAME,
-  uploadPreset: process.env.CLOUDINARY_UPLOAD_PRESET,
-  sources: ['local', 'url', 'camera', 'image_search', 'facebook', 'dropbox', 'instagram'],
-  showAdvancedOptions: false,
-  cropping: true,
-  multiple: false,
-  defaultSource: 'local',
-  styles: {
-    palette: {
-      window: '#FFFFFF',
-      windowBorder: '#95A5A6',
-      tabIcon: '#007BFF',
-      menuIcons: '#5A616A',
-      textDark: '#2C3E50',
-      textLight: '#FFFFFF',
-      link: '#007BFF',
-      action: '#E67E22',
-      inactiveTabIcon: '#BDC3C7',
-      error: '#E74C3C',
-      inProgress: '#007BFF',
-      complete: '#2ECC71',
-      sourceBg: '#ECF0F1'
-    },
-    fonts: {
-      default: null,
-      'system-ui, sans-serif': {
-        url: null,
-        active: true
-      }
-    }
-  }
-};
+// const handleSuccess = ({ info: { public_id } }) => {
+//   console.log('handleSuccess', public_id);
+// };
 
 export default Upload;
