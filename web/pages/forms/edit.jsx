847ea43115ad3ad1703f13cb60a1044d6787b107
John Kueh <yoongfook@gmail.com>
2019-05-05 23:52:26 +1000

    Can upload logos

--- web/pages/forms/edit.jsx
+++ web/pages/forms/edit.jsx
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 import gql from 'graphql-tag';
 import { useQuery, useMutation } from 'react-apollo-hooks';
 import Link from 'next/link';
+import { Image, Transformation } from 'cloudinary-react';
 import { withRouter } from 'next/router';
 import Alert from '../../components/alert-messages';
 import Layout from '../../layouts/logged-in';
+import { useUpload } from '../../lib/use-upload';
 
 export const FORM = gql`
   query($id: String!) {
@@ -24,6 +26,9 @@ export const UPDATE_FORM = gql`
     updateForm(input: $input) {
       id
       name
+      logos {
+        assetId
+      }
     }
   }
 `;
@@ -34,21 +39,36 @@ const FormEdit = ({
   }
 }) => {
   const {
-    data: { form },
+    data: { form = {} },
     loading
   } = useQuery(FORM, { variables: { id } });
   const [name, setName] = useState('');
   const [messages, setMessages] = useState(null);
   const update = useMutation(UPDATE_FORM);
+  const { openWidget } = useUpload(async publicId => {
+    try {
+      await update({
+        variables: {
+          input: {
+            id,
+            name,
+            logos: [publicId]
+          }
+        }
+      });
+    } catch (error) {
+      setMessages({ warning: error.graphQLErrors[0].extensions.exception.errors });
+    }
+  });
 
   useEffect(() => {
     !loading && setName(form.name);
-  }, [loading, form]);
+  }, [loading, form.name]);
 
   return (
     <Layout>
       <div className="row">
-        <div className="col-md-8">
+        <div className="col-md-12">
           <div className="mb-3">
             <Link href="/forms">
               <a href="/forms">Back to all forms</a>
@@ -78,6 +98,21 @@ const FormEdit = ({
               type="text"
               className="form-control mb-3"
             />
+            <div>
+              <button className="px-0 btn btn-link" type="button" onClick={openWidget}>
+                Upload logos
+              </button>
+              <div className="d-flex flex-wrap">
+                {form.logos &&
+                  form.logos.map(({ assetId }) => (
+                    <div key={assetId}>
+                      <Image width="400" className="mr-2 mb-2" publicId={assetId}>
+                        <Transformation width="800" crop="scale" />
+                      </Image>
+                    </div>
+                  ))}
+              </div>
+            </div>
             <button type="submit" className="mt-3 btn btn-primary">
               Update form
             </button>
